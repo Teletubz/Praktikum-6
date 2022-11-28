ce586887e994ad3c5b087cb89e2f6aa027819f8c
Gilang <gilangphb@gmail.com>
2022-11-28 21:14:48 +0700

    Menambahkan BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public abstract class BangunDatar {
+    
+    String warna;
+    
+    public String getWarna() {
+        return this.warna;
+    }
+    
+    public void setWarna() {
+        this.warna = warna;
+    }
+    
+    public abstract void gambar();
+    public abstract float luas();
+    
+}
